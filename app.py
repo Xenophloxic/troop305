310a0b4c070aff68087e0bff3cf43cbe26a59562
Xenophloxic <64376702+Xenophloxic@users.noreply.github.com>
2020-11-19 09:19:56 -0800

    added basic index

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    title = "Home"
+    return render_template("layout.html", title=title)
 
 if __name__ == "__main__":
     app.run(debug=True)
