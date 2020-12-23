c023263f783e4377f0294101d6921d7a1a0d0823
Xenophloxic <64376702+Xenophloxic@users.noreply.github.com>
2020-12-22 21:12:02 -0800

    finished index and login

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask_session import Session
 from helpers import login_required, SQL
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
+from tempfile import mkdtemp
 
 
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
@@ -12,6 +13,10 @@ ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 app = Flask(__name__)
 db = SQL('sqlite:///database.sqlite3')
 
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -32,73 +37,109 @@ def index():
     return render_template("index.html", title=title, images=img, year=datetime.now().year, indexabout=result, events=event)
 
 
-@app.route('/events', methods=['GET'])
+@app.route('/events', methods=["GET", "POST"])
 def events():
-    event = db.execute('SELECT * FROM events')
-    return render_template('events.html', year=datetime.now().year, events=event)
+    if request.method == "POST":
+        print('event post')
+    else:
+        event = db.execute('SELECT * FROM events')
+        return render_template('events.html', year=datetime.now().year, events=event)
 
 
-@app.route('/resource', methods=['GET'])
+@app.route('/resource', methods=["GET", "POST"])
 def resource():
     return "TODO resources"
 
 
-@app.route('/advance', methods=['GET'])
+@app.route('/advance', methods=["GET", "POST"])
 def advance():
     return "TODO advancement"
 
 
-@app.route('/photos', methods=['GET'])
+@app.route('/photos', methods=["GET", "POST"])
 def photos():
     return "TODO photos"
 
 
-@app.route('/docs', methods=['GET'])
+@app.route('/docs', methods=["GET", "POST"])
 def docs():
     return "TODO Docs"
 
 
-@app.route('/eagle', methods=['GET'])
+@app.route('/eagle', methods=["GET", "POST"])
 def eagle():
     return "TODO eagle"
 
 
-@app.route('/presource', methods=['GET'])
+@app.route('/presource', methods=["GET", "POST"])
 def presource():
     return "TODO Planning resources"
 
 
-@app.route('/contact', methods=['GET'])
+@app.route('/contact', methods=["GET", "POST"])
 def contact():
     return "TODO Contact us"
 
 
-@app.route('/policy', methods=['GET'])
+@app.route('/policy', methods=["GET", "POST"])
 def policy():
     return "TODO Policy"
 
 
-@app.route('/message', methods=['GET'])
+@app.route('/message', methods=["GET", "POST"])
 def message():
     return "TODO messaging services"
 
 
-@app.route('/about', methods=['GET'])
+@app.route('/about', methods=["GET", "POST"])
 def about():
     return "TODO About Us"
 
 
-@app.route('/news', methods=['GET'])
+@app.route('/news', methods=["GET", "POST"])
 def news():
     return "TODO News"
     
 
-@app.route('/webmaster')
+@app.route('/webmaster', methods=["GET", "POST"])
 def webhome():
     return render_template('webmaster.html', title="Webmaster", year=datetime.now().year)
 
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+
+    if request.method == "POST":
+
+        if not request.form.get("username"):
+            return "must provide username"
+
+        elif not request.form.get("password"):
+            return "must provide password"
+
+        else:
+            if request.form.get("username") == "webmaster":
+                if request.form.get("password") == "youshalln'tpass":
+                    session["user_id"] = 1
+                else:
+                    return "Incorrect information"
+
+        return redirect("/webmaster")
+
+    else:
+        return render_template("login.html", title="Log In")
+
+
+@app.route("/logout")
+def logout():
+
+    session.clear()
+
+    return redirect("/")
+
+
 @app.route('/upload', methods=['GET', 'POST'])
+@login_required
 def upload_file():
     if request.method == 'POST':
         if 'file' not in request.files:
@@ -124,9 +165,34 @@ def upload_file():
     return render_template('upload.html', year=datetime.now().year)
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    return "test"
+@app.route('/imgdel', methods=['GET', 'POST'])
+@login_required
+def imgdel():
+    return "Delete Images"
+
+
+@app.route('/editevent', methods=['GET', 'POST'])
+@login_required
+def editevent():
+    return "Edit Events"
+
+
+@app.route('/caledit', methods=['GET', 'POST'])
+@login_required
+def caledit():
+    return "Edit Calender"
+
+
+@app.route('/hra', methods=['GET', 'POST'])
+@login_required
+def hra():
+    return "Add Honor Roll"
+
+
+@app.route('/text', methods=['GET', 'POST'])
+@login_required
+def text():
+    return "Edit Text"
 
 
 if __name__ == "__main__":
--- static/script.js
+++ static/script.js
@@ -5,3 +5,28 @@ swal({
   button: "Ok, I'll check later",
 })
 
+function confirm(value) {
+  swal({
+    title: "Are you sure?",
+    text: `If you delete ${value}, it will be gone forever (A very long time!)`,
+    icon: "warning",
+    buttons: true,
+    dangerMode: true,
+  })
+  .then((willDelete) => {
+    if (willDelete) {
+      var xhr = new XMLHttpRequest();
+      xhr.open("POST", "http://127.0.0.1:5000/webmaster", true);
+      xhr.setRequestHeader('Content-Type', 'application/json');
+      xhr.send(JSON.stringify({
+          value: value
+      }));
+    } else {
+      swal("The file is safe", {
+        icon: "success",
+      });
+    }
+  });
+
+
+}
